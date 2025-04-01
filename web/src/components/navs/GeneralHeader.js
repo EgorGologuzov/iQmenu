d305bd226ba3ffc61f69b69ee4d58c98ef6c78c0
EgorGologuzov <gologuzovegor@gmail.com>
2025-04-01 21:39:12 +0500

    Implement GeneralLayout

---
+++ web/src/components/navs/GeneralHeader.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Logo from '../icons/Logo';
+import { Container, IconButton } from '@mui/material';
+
+function GeneralHeader() {
+  const title = "Заголовок страницы"
+  return (
+    <AppBar position="static">
+      <Container>
+        <Toolbar variant="dense">
+          <IconButton size="large" edge="start" color="inherit" aria-label="logo" sx={{ p: 1 }}>
+            <Logo />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div" sx={{ ml: 1, display: { xs: 'none', sm: 'block' } }}>
+            iQmenu
+          </Typography>
+          {title && <Typography variant="subtitle1" component="div" sx={{ ml: 1, display: { xs: 'none', sm: 'block' } }}>
+            /
+          </Typography>}
+          {title && <Typography variant="subtitle1" noWrap component="h1" sx={{ ml: 1, flexGrow: 1 }}>
+            { title }
+          </Typography>}
+          <Button color="inherit">Вход</Button>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+
+export default GeneralHeader
--- web/src/pages/layouts/GeneralLayout.js
+++ web/src/pages/layouts/GeneralLayout.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import { Outlet } from 'react-router'
-import PageHeader from '../../components/navs/PageHeader'
+import GeneralHeader from '../../components/navs/GeneralHeader'
+import GeneralFooter from '../../components/navs/GeneralFooter'
+import { Box } from '@mui/material'
 
 function GeneralLayout() {
   return (
-    <div>
-      <PageHeader />
-      <Outlet />
-    </div>
+    <Box sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+    }}>
+      
+      <GeneralHeader />
+
+      <Box component="main" sx={{ flexGrow: 1 }}>
+        <Outlet />
+      </Box>
+
+      <GeneralFooter />
+
+    </Box>
   )
 }
 
